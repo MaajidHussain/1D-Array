f969e15f97ffd6a47d1846c92b045ca7c7d2f6fb
Maajid Hussain <ksmaajidhussain@gmail.com>
2024-09-03 14:58:11 +0530

    Add files via upload

---
+++ creating_1D_Array.py
@@ -0,0 +1,9 @@
+from array import *
+
+#1.Creating an empty array of type Integers.
+empty_array=array('i')
+print(empty_array)
+
+#2.Creatinng an Array with Elements.
+my_array=array('i',[11,15,14,13])
+print(my_array)
