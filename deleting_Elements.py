dd4139bcbd4aa6920ffc18f9baf3ed6bafc8ee60
Maajid Hussain <ksmaajidhussain@gmail.com>
2024-10-23 20:30:37 +0530

    Update deleting_Elements.py

--- deleting_Elements.py
+++ deleting_Elements.py
@@ -1,3 +1,9 @@
+NOTE:-
+# Attempting to remove a non-existent element in the Array will lead to,
+# Array.remove(100)  # This will raise a ValueError
+# Use 3rd approach for this..
+--------------------------------------------------------------------------------------
+
 from array import *
 '''
 1.Deleting an element using element's value.
@@ -10,7 +16,7 @@ print(first_array)
 print(f"After removing Elements, size of the array is:",len(first_array))
 
 
-#---------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------
 
 from array import *
 '''
@@ -26,15 +32,27 @@ print(second_array)
 print(f"After deleting Elements, size of the array is:",len(second_array))
 
 
-#-----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------
+""" 3. Convert to List,
+       Remove Elements
+       Convert back to Array """
+from array import array
 
-from array import *
-'''
-3.Deleting an element using delete().
-'''
-third_array=array('i',[22,55,66,77,88,99,100,121,135])
-print(f"Before deleting Elements, size of the array is:",len(third_array),"\n")
-third_array.remove(121)
-third_array.remove(88)
+# Initializing the array
+third_array = array('i', [22, 55, 66, 77, 88, 99, 100, 121, 135])
+print(f"Before deleting elements, size of the array is: {len(third_array)}\n")
+
+# Deleting elements
+# First, we can convert the array to a list for easier manipulation
+temp_list = third_array.tolist()
+
+# Remove the elements you want to delete
+for element in [121, 88]:
+    if element in temp_list:
+        temp_list.remove(element)
+
+# Convert the list back to an array
+third_array = array('i', temp_list)
+
 print(third_array)
-print(f"After deleting Elements, size of the array is:",len(third_array))
+print(f"After deleting elements, size of the array is: {len(third_array)}")
